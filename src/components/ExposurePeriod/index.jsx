98e5bf53b6a2325de7dc611282a56b85e0763de6
gilmujjang <alsanrlf@naver.com>
2022-01-27 18:53:29 +0900

    feat: Exposure Period 레이아웃 작업 완료

--- src/components/ExposurePeriod/index.jsx
+++ src/components/ExposurePeriod/index.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+import * as S from "./styled";
 
 function ExposurePeriod() {
-  return <div>ExposurePeriod</div>;
+  return (
+    <>
+      <S.Title>노출 및 판매기간 설정</S.Title>
+      <table>
+        <tr>
+          <S.Content>상품 노출 기한</S.Content>
+          <S.SettingBox>
+            <S.RadioBox>
+              <S.RaidoBotton
+                type="radio"
+                value="no-limit"
+                id="no-limit"
+                name="no-limit"
+                checked
+              />
+              <S.Label>제한없음</S.Label>
+            </S.RadioBox>
+            <S.RadioBox>
+              <S.RaidoBotton
+                type="radio"
+                value="no-limit"
+                id="no-limit"
+                name="no-limit"
+              />
+              <S.Label>미노출</S.Label>
+            </S.RadioBox>
+            <S.RadioBox>
+              <S.RaidoBotton
+                type="radio"
+                value="no-limit"
+                id="no-limit"
+                name="no-limit"
+              />
+              <S.Label>노출 기간 설정</S.Label>
+            </S.RadioBox>
+            <div>달력</div>
+          </S.SettingBox>
+        </tr>
+        <tr>
+          <S.Content>상품 판매 기한</S.Content>
+          <S.SettingBox>
+            <S.RadioBox>
+              <S.RaidoBotton
+                type="radio"
+                value="no-limit"
+                id="no-limit"
+                name="no-limit"
+                checked
+              />
+              <S.Label>제한없음</S.Label>
+            </S.RadioBox>
+            <S.RadioBox>
+              <S.RaidoBotton
+                type="radio"
+                value="no-limit"
+                id="no-limit"
+                name="no-limit"
+              />
+              <S.Label>미노출</S.Label>
+            </S.RadioBox>
+            <S.RadioBox>
+              <S.RaidoBotton
+                type="radio"
+                value="no-limit"
+                id="no-limit"
+                name="no-limit"
+              />
+              <S.Label>노출 기간 설정</S.Label>
+            </S.RadioBox>
+            <div>달력</div>
+          </S.SettingBox>
+        </tr>
+      </table>
+    </>
+  );
 }
 
 export default ExposurePeriod;
---
+++ src/components/ExposurePeriod/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  padding: 8px;
+`;
+
+export const Content = styled.td`
+  font-size: 16px;
+  background-color: #cccccc;
+  margin: 0;
+  padding: 12px 8px 0 8px;
+  vertical-align: top;
+  min-width: 120px;
+`;
+
+export const SettingBox = styled.td`
+  padding: 8px;
+  max-width: 540px;
+`;
+
+export const RaidoBotton = styled.input`
+  type: radio;
+`;
+
+export const Label = styled.span``;
+
+export const RadioBox = styled.div`
+  margin: 4px;
+`;
