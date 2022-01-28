dfcad5cdb7e5743e5be4a7794d7c9f14f8dc84e7
gilmujjang <alsanrlf@naver.com>
2022-01-28 18:21:47 +0900

    refactor: date format 함수 utils화

--- src/components/Delivery/index.jsx
+++ src/components/Delivery/index.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
-import { Table, Title, Content, SettingBox } from "commons/Styled/styled";
+import {
+  Table,
+  Title,
+  Content,
+  SettingBox,
+  CalendarBox,
+  Text,
+} from "commons/Styled/styled";
+import Calendar from "commons/utils/Calendar";
+import { todayMaker, nextWeekMaker } from "commons/utils/DateMaker";
 import Toggle from "commons/utils/Toggle";
 
 function Delivery() {
-  const [toggle, setToggle] = useState(false);
+  const today = todayMaker;
+  const nextWeek = nextWeekMaker;
+  const [apoint, setApoint] = useState(false);
+  const [visit, setVisit] = useState(false);
+  const [preOrder, setPreOrder] = useState(false);
 
+  const [orderPeriod, setOrderPeriod] = useState({
+    startDate: today,
+    endDate: nextWeek,
+  });
+
+  console.log(orderPeriod);
   return (
     <Table>
       <tbody>
@@ -14,19 +33,37 @@ function Delivery() {
         <tr>
           <Content>사용자 배송일 출발일 지정</Content>
           <SettingBox>
-            <Toggle name="card" toggle={toggle} setToggle={setToggle} />
+            <Toggle name="apoint" toggle={apoint} setToggle={setApoint} />
           </SettingBox>
         </tr>
         <tr>
           <Content>방문 수령</Content>
           <SettingBox>
-            <Toggle name="card" toggle={toggle} setToggle={setToggle} />
+            <Toggle name="visit" toggle={visit} setToggle={setVisit} />
           </SettingBox>
         </tr>
         <tr>
           <Content>선 주문 예약 배송</Content>
           <SettingBox>
-            <Toggle name="card" toggle={toggle} setToggle={setToggle} />
+            <Toggle name="preOrder" toggle={preOrder} setToggle={setPreOrder} />
+            <CalendarBox>
+              <Text>주문 시간</Text>
+              <Calendar
+                date={orderPeriod}
+                setDate={setOrderPeriod}
+                dataType="startDate"
+                state={preOrder}
+                activeWord
+              />
+              <Text>~</Text>
+              <Calendar
+                date={orderPeriod}
+                setDate={setOrderPeriod}
+                dataType="endDate"
+                state={preOrder}
+                activeWord
+              />
+            </CalendarBox>
           </SettingBox>
         </tr>
       </tbody>
