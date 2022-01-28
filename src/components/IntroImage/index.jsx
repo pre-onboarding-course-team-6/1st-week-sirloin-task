0cc9b5b3d0cec301501d1a3dac36d77d497b8012
minbr0ther <minbr0ther@naver.com>
2022-01-29 06:39:19 +0900

    style: 상품소개 이미지, 구매자 추천 이미지 컴포넌트 분리

--- src/components/IntroImage/index.jsx
+++ src/components/IntroImage/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ImageAttachment from "../../commons/components/ImageAttachment";
 
 function IntroImage() {
-  return <div>IntroImage</div>;
+  return <ImageAttachment type="multiple">상품 소개 이미지</ImageAttachment>;
 }
 
 export default IntroImage;
--- src/components/RecommendImage/index.jsx
+++ src/components/RecommendImage/index.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import ImageAttachment from "../../commons/components/ImageAttachment";
 
 function RecommendImage() {
-  return (
-    <>
-      <ImageAttachment type="multiple">상품 소개 이미지</ImageAttachment>
-      <ImageAttachment type="multiple">구매자 추천 이미지</ImageAttachment>
-    </>
-  );
+  return <ImageAttachment type="multiple">구매자 추천 이미지</ImageAttachment>;
 }
 
 export default RecommendImage;
