720e94b2488b127aa280b11fb54b1531a4a3dfed
gilmujjang <alsanrlf@naver.com>
2022-01-28 11:32:43 +0900

    fate: 기타 설정 컴포넌트 구현

--- src/components/Etc/index.jsx
+++ src/components/Etc/index.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Toggle from "commons/utils/Toggle";
+import * as S from "./styled";
 
 function Etc() {
-  return <div>Etc</div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <S.Table>
+      <tbody>
+        <tr>
+          <S.Title colSpan="2">기타 설정</S.Title>
+        </tr>
+        <tr>
+          <S.Content>감사카드 제공</S.Content>
+          <S.SettingBox>
+            <Toggle name="card" toggle={toggle} setToggle={setToggle} />
+          </S.SettingBox>
+        </tr>
+      </tbody>
+    </S.Table>
+  );
 }
 
 export default Etc;
