c32fb9367c561cd1070abd1efeb62402734fbc36
minbr0ther <minbr0ther@naver.com>
2022-01-29 14:34:47 +0900

    feat: 상품 등록 메인 페이지 heder 작성

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,15 @@ import Notify from "components/Notify";
 import Option from "components/Option";
 import RecommendImage from "components/RecommendImage";
 import Delivery from "components/Delivery";
+import * as S from "styled";
 
 function App() {
   return (
-    <div>
+    <S.Main>
+      <S.Header>
+        <h3>상품 등록</h3>
+        <button type="button">저장하기</button>
+      </S.Header>
       <ExposureSellPeriod />
       <Info />
       <Option />
@@ -21,7 +26,7 @@ function App() {
       <Delivery />
       <Benefit />
       <Etc />
-    </div>
+    </S.Main>
   );
 }
 
